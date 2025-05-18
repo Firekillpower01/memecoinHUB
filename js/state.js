620f43f3d79dca51a3096bf91f4a9a59a7770a65
Firekillpower01 <davevanburken@gmail.com>
2025-05-18 19:52:40 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,15 +1,17 @@
 // js/state.js
 
+// App status & data
 export let memeBalance = 0;
+export let userWallet = null;
 
 export function setMemeBalance(amount) {
   memeBalance = amount;
 }
 
-export function addToMemeBalance(amount) {
+export function incrementMemeBalance(amount) {
   memeBalance += amount;
 }
 
-export function getMemeBalance() {
-  return memeBalance;
+export function setUserWallet(pubKey) {
+  userWallet = pubKey;
 }
