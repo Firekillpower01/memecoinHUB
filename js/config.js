f77aaeeaaf0c818ddebbebd4ec1efc929b54de9c
Firekillpower01 <davevanburken@gmail.com>
2025-05-18 22:50:19 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@
 
 // Instellingen en vaste waarden voor de app
 export const CONFIG = {
-  TOKEN_NAME: "$MEME",
-  TOKEN_INCREMENT: 100,     // Hoeveel tokens je krijgt bij mint
-  START_BALANCE: 0,         // Beginbalans bij het starten van de app
-  AIRDROP_AMOUNT: 50,       // Hoeveel tokens je krijgt bij een airdrop
-  SPIN_COST: 10,            // Kosten per spin in het slotspel
-  WIN_REWARD: 50            // Beloning als je wint bij slots
+  TOKEN_NAME: "$MEME",        // Naam van de token
+  TOKEN_INCREMENT: 100,       // Hoeveel tokens je krijgt bij mint
+  START_BALANCE: 0,           // Beginbalans bij het starten van de app
+  AIRDROP_AMOUNT: 50,         // Hoeveel tokens je krijgt bij een airdrop
+  AIRDROP_LIMIT: 100,         // Max. balans om in aanmerking te komen voor een airdrop
+  SPIN_COST: 10,              // Kosten per spin in het slotspel
+  WIN_REWARD: 50              // Beloning als je wint bij slots
 };
